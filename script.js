ef3c1ac223e2c90431cbd326d9a9c6636171c4e7
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-04-03 20:00:38 +0500

    Callback Hell

--- script.js
+++ script.js
@@ -3,17 +3,17 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+/*
 ///////////////////////////////////////
 // https://api-mobilespecs.azharimm.site/v2/brands/apple-phones-48?page=2
-const getCountryData = country => {
+const getCountryDataAndNeighbour = function (country) {
   const request = new XMLHttpRequest();
 
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
   request.send();
-  console.log(request.responseText);
 
   request.addEventListener('load', function () {
-    const [data] = JSON.parse(request.responseText);
+    const [data] = JSON.parse(this.responseText);
     console.log(data);
 
     const html = `
@@ -25,7 +25,7 @@ const getCountryData = country => {
         <p class="country__row"><span>👫</span>${(
           +data.population / 1000000
         ).toFixed(1)} people</p>
-        <p class="country__row"><span>🗣️</span>${data.languages[0]} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0]}</p>
         <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
       </div>
     </article>`;
@@ -38,3 +38,72 @@ const getCountryData = country => {
 getCountryData('uzbekistan');
 getCountryData('portugal');
 getCountryData('germany');
+*/
+
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages}</p>
+        </p>
+        <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
+      </div>
+    </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2[0], 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('afghanistan');
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('1 second passed');
+    setTimeout(() => {
+      console.log('1 second passed');
+      setTimeout(() => {
+        console.log('1 second passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
