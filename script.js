56dd18124c1b29f328c78e675eff90f026fc5afe
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-04-03 18:06:49 +0500

    Get country data

--- script.js
+++ script.js
@@ -4,3 +4,37 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+// https://api-mobilespecs.azharimm.site/v2/brands/apple-phones-48?page=2
+const getCountryData = country => {
+  const request = new XMLHttpRequest();
+
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  console.log(request.responseText);
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(request.responseText);
+    console.log(data);
+
+    const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0]} people</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
+      </div>
+    </article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('uzbekistan');
+getCountryData('portugal');
+getCountryData('germany');
